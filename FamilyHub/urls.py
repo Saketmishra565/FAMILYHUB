446299fd30b4391d6537aa1544971532d86e7cef
saket mishra <sakkumishra1996@gmail.com>
2025-10-05 18:05:41 +0530

    Fix: Stable Python runtime and cleaning requirements.txt

--- FamilyHub/urls.py
+++ FamilyHub/urls.py
@@ -1,10 +1,8 @@
-# FamilyHub/urls.py (CORRECTED)
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth import views as auth_views # Login/Logout के लिए
+from django.contrib.auth import views as auth_views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,25 +12,19 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'), 
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
-    # App URLs - 🌟 NOTE: Added 'namespace' to all app includes 🌟
-    path('', include('core.urls')), # Home Page
-    
-    # gallery में namespace पहले से था, यह सही है
+    # App URLs - All namespaces are correct
+    path('', include('core.urls')), 
     path('gallery/', include('gallery.urls', namespace='gallery')), 
-    
-    # blog में namespace जोड़ा गया
     path('blog/', include('blog.urls', namespace='blog')),
-    
-    # calendar_app में namespace जोड़ा गया
     path('calendar/', include('calendar_app.urls', namespace='calendar_app')),
-    
-    # contacts में namespace जोड़ा गया
     path('contacts/', include('contacts.urls', namespace='contacts')),
-    
-    # recipes में namespace जोड़ा गया
     path('recipes/', include('recipes.urls', namespace='recipes')),
 ]
 
-# Media Files in Development
+# 🟢 CRITICAL FIX: STATIC AND MEDIA FILES IN DEVELOPMENT MODE 🟢
 if settings.DEBUG:
+    # 🌟 THIS LINE IS CRITICAL FOR CSS/JS TO LOAD 🌟
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    
+    # Media Files (Images, etc.)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
